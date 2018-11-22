0acd0daede849757bf82937f51a294507b3a675e
yiyun <tp91225@163.com>
2018-11-22 15:35:54 +0800

    new repo

---
+++ js/showPic.js
@@ -0,0 +1,14 @@
+function showPic(which) {
+    var source=which.getAttribute("href");
+    var placeholder=document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+
+    var text=which.getAttribute("title");
+    var des=document.getElementById("description");
+    des.firstChild.nodeValue=text;
+}
+function countBodyChildren(){
+    var body_element=document.getElementsByTagName("body")[0];
+    alert(body_element.childNodes.length)
+}
+// window.onload=countBodyChildren;
