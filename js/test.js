d1b0889cb6952993e879193b0c29ae361ac4bd7e
yiyun <tp91225@163.com>
2018-11-25 16:57:01 +0800

    add check method

--- js/test.js
+++ js/test.js
@@ -3,6 +3,8 @@ function popUp(url) {
     window.open(url,"popUp","wigth=320,height=480");
 }
 function prearelink(params) {
+    // alert(document.getElementById)
+    if(!document.getElementsByTagName||document.getElementById) return false;//对象检测有更好的向后兼容性
     var links=document.getElementsByTagName("a");
     for (var i=0;i<links.length;i++){
         if(links[i].getAttribute("class")=="popUp"){
