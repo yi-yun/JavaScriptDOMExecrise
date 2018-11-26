424933499eb0ae6404260a63c7a66aeda131975a
yi-yun <tp91225@163.com>
2018-11-26 22:25:01 +0800

    pratice

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 ### 参考书籍
 - [JavaScript DOM编程艺术](https://book.douban.com/subject/6038371/)
+- [书中地址失效，大佬的示例代码](https://github.com/Doun2017/StudyJavaScriptCode_DOMArtBook)
--- js/showPic.js
+++ js/showPic.js
@@ -12,4 +12,30 @@ function countBodyChildren(){
     var body_element=document.getElementsByTagName("body")[0];
     alert(body_element.childNodes.length)
 }
-// window.onload=countBodyChildren;
+function prepareGallery() {
+    alert("tets")
+    if (!document.getElementById) return false;
+    if (!document.getElementsByTagName) return false;
+    var image = document.getElementById("imageGallery");
+    if (!image) return false;
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++){
+        links[i].onclick=function () {
+            showPic(this);
+            return false;
+        }
+    }
+}
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload=func
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
