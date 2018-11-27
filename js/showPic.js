9a57156412b6ab7956dbf94f1be89b11759071db
yiyun <tp91225@163.com>
2018-11-27 15:31:38 +0800

    add new pratice

--- js/showPic.js
+++ js/showPic.js
@@ -1,19 +1,26 @@
 function showPic(which) {
     var source=which.getAttribute("href");
     var placeholder=document.getElementById("placeholder");
+    if(!placeholder) return false;
+    if(placeholder.nodeName!="IMG") return false;//添加检查
     placeholder.setAttribute("src",source);
 
-    var text1=which.getAttribute("title");
     // alert(text1);
+    //平稳退化
     var des=document.getElementById("description");
-    des.firstChild.nodeValue=text1;
+    if(des){
+        var text1=which.getAttribute("title")?which.getAttribute("title"):"";
+        if(des.firstChild.nodeType==3){
+            des.firstChild.nodeValue=text1;
+        }
+    }
+    return true;
 }
 function countBodyChildren(){
     var body_element=document.getElementsByTagName("body")[0];
     alert(body_element.childNodes.length)
 }
 function prepareGallery() {
-    alert("tets")
     if (!document.getElementById) return false;
     if (!document.getElementsByTagName) return false;
     var image = document.getElementById("imageGallery");
@@ -21,9 +28,11 @@ function prepareGallery() {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++){
         links[i].onclick=function () {
-            showPic(this);
-            return false;
+            // showPic(this);
+            // return false;
+            return !showPic(this);
         }
+        // links[i].onkeypress=links[i].onclick;//决定不添加,按任何键都会切换图片,onclick 可以满足 Tab + Enter 的选择 
     }
 }
 function addLoadEvent(func) {
